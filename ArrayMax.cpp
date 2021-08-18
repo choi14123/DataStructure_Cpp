39d5a5f91503deb66c205a5ba8eaba3b4dd1e882
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-08-18 19:46:08 +0900

    add day 08 18

---
+++ ArrayMax.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int ArrayMax(int score[], int n)
+    {
+         int tmp = score[0];
+          for (int i =1; i < n; i++){
+               if (score [i] > tmp){
+                    tmp = score [i];
+               }
+          }
+          return tmp;
+    }
