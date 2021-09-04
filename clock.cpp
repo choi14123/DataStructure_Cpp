ff2632bb92a6584de882d29d4d555af71cf022dd
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-09-04 10:03:21 +0900

    add day 09 04

--- clock.cpp
+++ clock.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
 int main( void )
@@ -9,6 +10,11 @@ int main( void )
     clock_t start, finish;
     double duration;
     start = clock();
+    int n;
+	cin >> n;
+	for(int i = 1 ; i <= n ; i++)
+	for(int j = 1 ; j <= i ; j++) 
+	
     finish = clock();
     duration = (double)(finish - start) / CLOCKS_PER_SEC;
     cout << duration << "초입니다. " << endl;
