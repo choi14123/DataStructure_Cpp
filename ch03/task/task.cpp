440afdf603504d8869a89d9895634374d5f94f38
Choi Chang Hwan <ckdghks5565@gmail.com>
2021-10-02 21:26:28 +0900

    add day 10 02

--- ch03/task/task.cpp
+++ ch03/task/task.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 
 int main(){
-    long lArray[20];
+    long lArray[20] = {};
 
     long(*p)[20] = &lArray;
+    
 
     (*p)[3] = 300;
 
     cout << "lArray[3] = " << lArray[3] << endl;
-
+    for (int i = 0; i < 20; ++i)
+    {
+    cout << "(*p)[" << i <<"] = " << (*p)[i] << endl;
+    }
     return 0;
 }
